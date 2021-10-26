8b8c78b61a08c8b29c65c9d8ed74ad44bba56bbf
MrWilsonXu <hrxspp@126.com>
2021-10-26 22:48:10 +0800

    添加配置

--- TEST/AppDelegate.swift
+++ TEST/AppDelegate.swift
@@ -16,7 +16,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let vc = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateViewController(identifier: "ViewController")
         self.window?.rootViewController = UINavigationController(rootViewController: vc)
         print(NSHomeDirectory())
+        requestBaidu()
+        print("------------默认urlCache大小---------");
+        printCacheSize()
+        initNSUrlCache()
+        print("------------设置的urlCache大小---------");
+        printCacheSize()
         return true
+    }
+    
+    func initNSUrlCache() {
+        let urlCache = URLCache(memoryCapacity: 4 * 1024 * 1024, diskCapacity: 20 * 1024 * 1024, diskPath: nil);
+    }
+    
+    func printCacheSize() {
+        print(URLCache.shared.diskCapacity)
+        print(URLCache.shared.memoryCapacity)
+    }
+    
+    func requestBaidu() {
+        let url = URL(string: "https://www.baidu.com")!
+        let request = URLRequest(url: url, cachePolicy: URLRequest.CachePolicy.useProtocolCachePolicy, timeoutInterval: 60);
+        let session = URLSession.shared
+        let task = session .dataTask(with: request)
+        task.resume()
     }
 }
 
